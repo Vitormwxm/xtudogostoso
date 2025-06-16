2651fb687a09df4b47ddd644b2db95acdb78c4bf
Vitor Martiniano <vitormwxm@gmail.com>
2025-06-16 19:34:07 -0300

    adicionando dados da página

---
+++ data/burguers.php
@@ -0,0 +1,31 @@
+<?php
+$burgers = [
+    [
+        'id' => 1,
+        'title' => 'Bruto Burger',
+        'description' => 'Pão brioche, smash duplo, queijo cheddar, bacon crocante e molho da casa.',
+        'tags' => ['smash', 'bacon', 'artesanal'],
+        'img' => 'burguer1.png'
+    ],
+    [
+        'id' => 2,
+        'title' => 'TNT Bacon',
+        'description' => 'Smash triplo, muito bacon, cheddar derretido e molho picante.',
+        'tags' => ['picante', 'bacon', 'smash'],
+        'img' => 'burguer1.png'
+    ],
+    [
+        'id' => 3,
+        'title' => 'American Dream',
+        'description' => 'Carne alta, cheddar, onion rings, barbecue e pão australiano.',
+        'tags' => ['gourmet', 'onion rings', 'barbecue'],
+        'img' => 'burguer1.png'
+    ],
+    [
+        'id' => 4,
+        'title' => 'Coração de Ogro',
+        'description' => 'Smash triplo, queijo prato, bacon infinito e maionese verde.',
+        'tags' => ['smash', 'bacon', 'monstruoso'],
+        'img' => 'burguer1.png'
+    ],
+];
