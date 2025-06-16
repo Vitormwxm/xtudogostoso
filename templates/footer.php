6b78e4895410fe20f94924dcce81937877956675
Vitor Martiniano <vitormwxm@gmail.com>
2025-06-16 19:35:54 -0300

    includes do projeto

---
+++ templates/footer.php
@@ -0,0 +1,8 @@
+
+
+
+    <footer>
+         Vitor Martiniano
+    </footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,22 @@
+<?php 
+    include_once ($_SERVER['DOCUMENT_ROOT'] . "/xtudogostoso/helpers/url.php");
+    include_once($_SERVER['DOCUMENT_ROOT'] . "/xtudogostoso/data/burguers.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/style/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+    <title>X-TUDOGOSTOSO</title>
+</head>
+<body>  
+    <header>
+        <figure>
+            <img class="logo" src="<?= $BASE_URL?>/img/tudogostoso.png" alt="logo TUDOGOSTOSO">
+        </figure>
+    </header>
